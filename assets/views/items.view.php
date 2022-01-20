70bbb1f2d64312809dc95697bc3475624f9ea176
Juraia Pinas <byenna21@gmail.com>
2022-01-20 18:15:44 +0100

    tidy op code

--- assets/views/items.view.php
+++ assets/views/items.view.php
@@ -1,40 +1,33 @@
 
 <div id="allItems">
     <section id="form-container" class="container">
-    <div class="form_view"><div class="text_cart" class="mt-7 mb-0"></div><div>
+        <div class="form_view">
+            <div class="text_cart" class="mt-7 mb-0"></div>
 
-        <div class="row justify-content-center">
-            
-                
-            <h1>De producten</h1>
-            <p>Hieronder vindt u een aantal producten die op dit moment in onze voedselbos verkrijgbaar zijn. Naast deze producten hebben we ook kruiden, paddestoelen en zaden verkrijgbaar. Een van onze medewerkers helpt u graag verder mocht u hierin interesse hebben.</p>
-            
-            <?php foreach($fruits as $fruit) : ?>
+                <div class="row justify-content-center">
+                                   
+                    <h1>De producten</h1>
+                    <p>Hieronder vindt u een aantal producten die op dit moment in onze voedselbos verkrijgbaar zijn. Naast deze producten hebben we ook kruiden, paddestoelen en zaden verkrijgbaar. Een van onze medewerkers helpt u graag verder mocht u hierin interesse hebben.</p>
+                        
+                        <?php foreach($fruits as $fruit) : ?>
 
-            <div class="card" style="width: 18rem;">
-            <img src="<?= $_ENV['IMAGE_PATH'] . $fruit['image']?>" class="card-img-top" alt= "<?= $fruit['title']?>">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= $_ENV['IMAGE_PATH'] . $fruit['image']?>" class="card-img-top" alt= "<?= $fruit['title']?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $fruit['title'] ?></h5>
+                            <p class="card-text"><?= $fruit['description'] ?></p>
+                            <p class="card-text">Prijs: €  <?= $fruit['price'] ?></p>
+                            <!-- <a href="#" class="buttonItem btn btn-primary">Bestel</a> -->
+                            <!-- <h6><br /><a href="index.php?fruit=<?= $fruit['id']; ?>" class="moreInfo">MEER INFO</a></h6> -->
+                            <input type="hidden" name="id_to_add" value="<?= $fruit['id'] ?>">
+                            <input @click="addToCart(<?= $fruit['id'] ?>)" class="buttonItem btn btn-primary" type="button" name="add" value="in winkelmandje" class="btn brand z-depth-0">
+                        </div>
+                    </div>
 
-            <div class="card-body">
-            <h5 class="card-title"><?= $fruit['title'] ?></h5>
-            <p class="card-text"><?= $fruit['description'] ?></p>
-            <p class="card-text">Prijs: €  <?= $fruit['price'] ?></p>
-            <!-- <a href="#" class="buttonItem btn btn-primary">Bestel</a> -->
-            <!-- <h6><br /><a href="index.php?fruit=<?= $fruit['id']; ?>" class="moreInfo">MEER INFO</a></h6> -->
+                        <?php endforeach; ?>
 
-
-            <input type="hidden" name="id_to_add" value="<?= $fruit['id'] ?>">
-            <input @click="addToCart(<?= $fruit['id'] ?>)" class="buttonItem btn btn-primary" type="button" name="add" value="in winkelmandje" class="btn brand z-depth-0">
-
+                </div>
             </div>
-            </div>
-
-
-            <?php endforeach; ?>
-            </div>
-            </div>
-
-
-            
         </div> 
     </section>
 </div>
